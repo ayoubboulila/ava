64adf599c62a649f82fdf146a497529c41084960
Ayoub Boulila <ayoub.boulila@esi-group.com>
2018-01-31 18:45:59 +0100

    added resources

--- dc2_test.py
+++ dc2_test.py
@@ -11,8 +11,15 @@ if __name__ == '__main__':
     # if you want to use BOARD numbering do this: "with AMSpi(True) as amspi:"
     with AMSpi() as amspi:
         # Set PINs for controlling shift register (GPIO numbering) add D7 -> pin 12 (GPIO 26)
+        # pin 21 [GPIO 29] -> D12
+        # pin 20 [GPIO 28] -> D4
+        # pin 16 [GPIO 27] -> D8
+        # pin 12 [GPIO 26] -> D7
         amspi.set_74HC595_pins(21, 20, 16)
         # Set PINs for controlling all 4 motors (GPIO numbering)
+        
+        # pin 5 [GPIO 21] -> D11 (PMW motor 1)
+        # pin 6 [GPIO 22] -> D3 (PMW motor 2)
         amspi.set_L293D_pins(PWM2A=5, PWM2B=6)
         print("GO: clockwise")
         amspi.run_dc_motors([amspi.DC_Motor_1, amspi.DC_Motor_2])
