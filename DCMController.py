64378dfbb6830b882b94d7a5c98edfa5c13b4087
Ayoub <ayoubboulila@gmail.com>
2018-02-14 18:18:31 +0000

    adding json input

--- DCMController.py
+++ DCMController.py
@@ -8,7 +8,8 @@ Created on 14 FEB. 2018
 import redis
 from datetime import datetime
 import traceback
-
+import json
+from pprint import pprint
 
 DCM_CH = 'DCMC'
 
@@ -27,8 +28,10 @@ try:
     
     while True:
         message = sub.get_message()
-        if message:
-            data = message['data']
+        if message and not isinstance(message['data'], int):
+            print(type(message['data']))
+            data = json.loads(message['data'].decode('utf-8'))
+            pprint(data)
             action = data['action']
             clockwise = data['clockwise']
             speed = data['speed']
@@ -41,12 +44,12 @@ except Exception as ex:
     print("exception in DCMController")
     traceback.print_exc()
 
-while True:
-    message = sub.get_message()
-    if message:
-        local_now = datetime.now()
-        print("local now: {}".format(local_now))
-        print("remote time: {}".format(message))
+##while True:
+##    message = sub.get_message()
+##    if message:
+##        local_now = datetime.now()
+##        print("local now: {}".format(local_now))
+##        print("remote time: {}".format(message))
         
         
         
