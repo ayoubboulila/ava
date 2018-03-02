a16d24cdf6d18013c1bbb5af706fcad7c2ea2048
Ayoub Boulila <ayoub.boulila@esi-group.com>
2018-03-02 18:49:16 +0100

    added TTS controller

---
+++ TTSController.py
@@ -0,0 +1,63 @@
+'''
+Created on 2 mars 2018
+
+@author: AYB
+'''
+
+
+import sys, os
+import signal
+import redis
+import Logger
+import subprocess
+import json
+
+
+# json =  '{"action": "speak",  "sentence": ""}'
+
+log = Logger.RCLog('TTSController')
+TTSC_CH = 'TTSC'
+
+class TTS:
+    _BIN_ = '/home/pi/builded_backup/mimic/mimic'
+    _VOICE_ = 'slt'
+    
+    
+    
+    def __init__(self, BIN =_BIN_, VOICE =_VOICE_):
+        self._BIN_PATH_ = BIN
+        self._VOICE_ = VOICE
+        
+    
+    def build_args(self):
+        args = [self._BIN_, '-voice', self._VOICE_, '--setf', 'duration_stretch=1.5' , '-t']
+        return args    
+        
+    def speak(self, sentence):
+        subprocess.check_output(self.build_args() + [sentence])
+    
+
+
+
+
+
+def main():
+    try:
+        broker = redis.StrictRedis()
+        sub = broker.pubsub()
+        sub.subscribe(TTSC_CH)
+        tts = TTS()
+        while True:
+            message = sub.get_message()
+            if message and not isinstance(message['data'], int) and message['type'] == 'message':
+                log.debug(type(message['data']))
+                data = json.loads(message['data'].decode('utf-8'))
+                log.debug("TTSC: received data:")
+                log.debug(data)
+                action = data['action']
+                sentence = data['sentence']
+                tts.speak(sentence)
+                
+    except Exception as ex:
+        log.error("error in TTSController")
+        log.error(ex, exc_info=True)
