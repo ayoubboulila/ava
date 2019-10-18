b56dd086627233b88fd51b93a1138f41de66155f
Ayoub Boulila <ayoub.boulila@esi-group.com>
2019-10-18 15:42:03 +0100

    added threaded camera get and show

---
+++ lib/ai/detector/threaded_detector.py
@@ -0,0 +1,42 @@
+'''
+Created on 18 oct. 2019
+
+@author: AYB
+'''
+import argparse
+import cv2
+from lib.cam.camera_threaded.CountsPerSec import CountsPerSec
+from lib.cam.camera_threaded.VideoGet import VideoGet
+from lib.cam.camera_threaded.VideoShow import VideoShow
+
+def putIterationsPerSec(frame, iterations_per_sec):
+    """
+    Add iterations per second text to lower-left corner of a frame.
+    """
+
+    cv2.putText(frame, "{:.0f} iterations/sec".format(iterations_per_sec),
+        (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255))
+    return frame
+
+def CameraThreads(source=0):
+    """
+    Dedicated thread for grabbing video frames with VideoGet object.
+    Dedicated thread for showing video frames with VideoShow object.
+    Main thread serves only to pass frames between VideoGet and
+    VideoShow objects/threads.
+    """
+
+    video_getter = VideoGet(source).start()
+    video_shower = VideoShow(video_getter.frame).start()
+    cps = CountsPerSec().start()
+
+    while True:
+        if video_getter.stopped or video_shower.stopped:
+            video_shower.stop()
+            video_getter.stop()
+            break
+
+        frame = video_getter.frame
+        frame = putIterationsPerSec(frame, cps.countsPerSec())
+        video_shower.frame = frame
+        cps.increment()
