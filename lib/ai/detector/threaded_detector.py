ccaa431c69d9bb6cfd33a5b7deff349cc7f5ceb5
Ayoub Boulila <ayoub.boulila@esi-group.com>
2019-10-18 16:20:19 +0100

    added detection

--- lib/ai/detector/threaded_detector.py
+++ lib/ai/detector/threaded_detector.py
@@ -3,13 +3,37 @@ Created on 18 oct. 2019
 
 @author: AYB
 '''
-import argparse
+from __future__ import print_function
+from imutils.video import WebcamVideoStream
+from imutils.video import FPS
+import imutils
+from os import path
+import time
+import logging
+import sys
+import numpy as np
 import cv2
-import sys 
+from object_detector_detection_api import ObjectDetectorDetectionAPI
+from object_detector_detection_api_lite import ObjectDetectorLite
+from utils.utils import Models
+
 sys.path.append("/home/pi/ava")
 from lib.cam.camera_threaded.CountsPerSec import CountsPerSec
 from lib.cam.camera_threaded.VideoGet import VideoGet
 from lib.cam.camera_threaded.VideoShow import VideoShow
+
+
+logging.basicConfig(
+    stream=sys.stdout,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt=' %I:%M:%S ',
+    level="INFO"
+)
+logger = logging.getLogger('detector')
+
+
+basepath = path.dirname(__file__)
+
 
 def putIterationsPerSec(frame, iterations_per_sec):
     """
@@ -19,8 +43,18 @@ def putIterationsPerSec(frame, iterations_per_sec):
     cv2.putText(frame, "{:.0f} iterations/sec".format(iterations_per_sec),
         (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255))
     return frame
+def detect(predictor, image):
+    result = predictor.detect(image)
 
-def CameraThreads(source=0):
+    for obj in result:
+        logger.info('coordinates: {} {}. class: "{}". confidence: {:.2f}'.
+                        format(obj[0], obj[1], obj[3], obj[2]))
+
+        cv2.rectangle(image, obj[0], obj[1], (0, 255, 0), 2)
+        cv2.putText(image, '{}: {:.2f}'.format(obj[3], obj[2]),
+                        (obj[0][0], obj[0][1] - 5),
+                        cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
+def CameraThreads(predictor, source=0):
     """
     Dedicated thread for grabbing video frames with VideoGet object.
     Dedicated thread for showing video frames with VideoShow object.
@@ -31,6 +65,7 @@ def CameraThreads(source=0):
     video_getter = VideoGet(source).start()
     video_shower = VideoShow(video_getter.frame).start()
     cps = CountsPerSec().start()
+#    fps = FPS().start()
 
     while True:
         if video_getter.stopped or video_shower.stopped:
@@ -40,8 +75,19 @@ def CameraThreads(source=0):
 
         frame = video_getter.frame
         frame = putIterationsPerSec(frame, cps.countsPerSec())
-        video_shower.frame = frame
+        out_frame = detect(predictor, frame)
+        video_shower.frame = out_frame
         cps.increment()
 
+
+
+
 if __name__ == "__main__":
-    CameraThreads()
+        # initialize detector
+    logger.info('Model loading...')
+#    Models.from_string('tf_lite')
+    predictor = ObjectDetectorLite()
+#    predictor = ObjectDetectorDetectionAPI(path.join(basepath, "frozen_inference_graph.pb"))
+
+    print("[INFO] sampling THREADED frames from webcam...")
+    CameraThreads(predictor)
